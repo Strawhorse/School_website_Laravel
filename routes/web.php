31dc6def7fba2b3618b9c8aeabfab15b4f0e8ab4
Sean B <strawhorse@gmail.com>
2023-09-10 13:46:36 +0100

    Creating first routes for main pafge

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    return view('about');
+});
+
+Route::get('/curriculum', function() {
+    return view('about');
+});
+
+Route::get('/queries', function() {
+    return view('about');
+});
+
+Route::get('/login', function() {
+    return view('about');
+});
+
+
+
+
+
+
+
+
